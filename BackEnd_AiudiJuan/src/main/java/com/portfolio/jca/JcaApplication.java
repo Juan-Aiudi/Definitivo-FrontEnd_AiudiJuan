2fd2892f21549d7eefe4fce4ad2df7a01c9de19d
Juan Carlos Aiudi <94766344+Juan-Aiudi@users.noreply.github.com>
2022-05-21 16:23:13 -0300

    Desarrollo Back

---
+++ BackEnd_AiudiJuan/src/main/java/com/portfolio/jca/JcaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.jca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcaApplication.class, args);
+	}
+
+}
